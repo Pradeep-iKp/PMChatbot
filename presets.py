c65f24f7579bc5a2c798d22ee9abd4f39d454a03
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-11-19 14:24:12 +0530

    .

--- presets.py
+++ presets.py
@@ -1,7 +1,6 @@
 #-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 class Presets(object):
-    WELCOME_TEXT = "Hello, <b>{}</b>\nYσυ cαη тαłk/cσηтαcт мy мαsтєя вy υsıηg мє.\n• вєттєя υsє єηgłısн!\n• Stay on Point.\n\n~ [@HKrrish] 🧑‍💻
-.."
+    WELCOME_TEXT = "Hello, <b>{}</b>\nYσυ cαη тαłk/cσηтαcт мy мαsтєя вy υsıηg мє.\n• вєттєя υsє єηgłısн!\n• Stay on Point.\n\n~ [@HKrrish] 🧑‍💻\n.."
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
